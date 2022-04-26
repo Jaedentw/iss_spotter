5e688862c58354540e21fea80c82987d4a3c3bf5
Jaeden West <jaeden.t.west@gmail.com>
2022-04-26 17:05:28 -0600

    revised assignment, moved code from index to iss

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 // index.js
 const { fetchMyIP } = require('./iss');
 
-fetchMyIP((error, response, ip) => {
+fetchMyIP((error, ip) => {
   if (error) {
-    callback(error, null);
-    return;
-  }
-  if (response.statusCode !== 200) {
-    const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-    callback(Error(msg), null);
+    console.log("It didn't work!" , error);
     return;
   }
   console.log('It worked! Returned IP:' , ip);
--- iss.js
+++ iss.js
@@ -2,6 +2,14 @@ const request = require("request");
 
 const fetchMyIP = function(callback) {
   request.get('https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_7tQmAajv4oYpoMmTWcLDe1KyJsY0h&ipAddress',(error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      return;
+    }
     const pBody = JSON.parse(body);
     const ip = pBody['ip'];
     callback(error, response, ip);
