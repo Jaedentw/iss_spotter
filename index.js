3aac1b5343dfcccef2bdc9e22b9e91de8775afcf
Jaeden West <jaeden.t.west@gmail.com>
2022-04-27 12:45:34 -0600

    unrefined coords fetcher functional

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // index.js
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
-  console.log('It worked! Returned IP:' , ip);
-});
-
-// fetchCoordsByIP((error, coords) => {
+// fetchMyIP((error, ip) => {
 //   if (error) {
 //     console.log("It didn't work!" , error);
 //     return;
 //   }
-//   console.log('It worked! Returned coords:' , coords);
-// })
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+fetchCoordsByIP('174.56.88.131', (error, coords) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+  console.log('It worked! Returned coords:' , coords);
+})
