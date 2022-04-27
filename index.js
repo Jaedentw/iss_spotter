b153f252f4673a91087d194f16d740603144c4f8
Jaeden West <jaeden.t.west@gmail.com>
2022-04-27 12:54:50 -0600

    linted and functional: fetchCoords, fetchIP

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // index.js
 const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-//   console.log('It worked! Returned IP:' , ip);
-// });
-
-fetchCoordsByIP('174.56.88.131', (error, coords) => {
+fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
+  console.log('It worked! Returned IP:' , ip);
+});
+
+fetchCoordsByIP('174.56.88.131', (error, coords) => {
+  if (error) {
+    console.log("It didn't work!" , error['message']);
+    return;
+  }
   console.log('It worked! Returned coords:' , coords);
-})
+});
